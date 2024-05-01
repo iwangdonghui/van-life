1ece3e8b8ec56191f89ced2b2f7a633cc3013de5
wangdonghui <wangdonghuiibt@gmail.com>
2024-05-01 13:46:01 +0800

    Update A11y
    Updating A11y for the clickable area; Changing h3 tag to h2 to avoid tag jumping.

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -11,10 +11,10 @@ export default function Vans() {
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
-                <img src={van.imageUrl} />
+            <Link to={`/vans/${van.id}`} aria-label={`View details for ${van.name}, priced at $${van.price} per day`}>
+                <img src={van.imageUrl} alt={`Image of ${van.name}`}/>
                 <div className="van-info">
-                    <h3>{van.name}</h3>
+                    <h2>{van.name}</h2>
                     <p>${van.price}<span>/day</span></p>
                 </div>
                 <i className={`van-type ${van.type} selected`}>{van.type}</i>

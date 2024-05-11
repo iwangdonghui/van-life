df7d7e42f9a3861fc9a267b46d7a5723d87e6aef
wangdonghui <wangdonghuiibt@gmail.com>
2024-05-11 13:55:02 +0800

    merged search params with setSearchParams
    Update onClick function to merge the search params and setSearchParams fuction & Conditional rendering the clear filter button.

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -27,26 +27,42 @@ export default function Vans() {
         </div>
     ))
 
+    function handleFilterChange (key, value) {
+        setSearchParams(prevParams => {
+            if (value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
+
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
                 <button 
-                    onClick={() => setSearchParams({type: "simple"})}
+                    onClick={() => handleFilterChange("type", "simple")}
                     className="van-type simple"
                 >Simple</button>
                 <button 
-                    onClick={() => setSearchParams({type: "luxury"})}
+                    onClick={() => handleFilterChange("type", "luxury")}
                     className="van-type luxury"
                 >Luxury</button>
                 <button 
-                    onClick={() => setSearchParams({type: "rugged"})}
+                    onClick={() => handleFilterChange("type", "rugged")}
                     className="van-type rugged"
                 >Rugged</button>
-                <button 
-                    onClick={() => setSearchParams({})}
-                    className="van-type clear-filters"
-                >Clear filter</button>
+                {
+                    typeFilter ?    
+                    (<button 
+                        onClick={() => handleFilterChange("type", null)}
+                        className="van-type clear-filters"
+                    >Clear filter</button>)
+                    : null
+                }
+                
             
             </div>
             <div className="van-list">

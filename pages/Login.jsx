874b6d04873acb6d4167104b5b9c12c244e58cd2
wangdonghui <wangdonghuiibt@gmail.com>
2024-05-17 15:45:03 +0800

    better auth but still fake

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -11,7 +11,7 @@ export async function action ({ request }) {
     const email = formData.get("email")
     const password = formData.get("password")
     const data = await loginUser({ email, password })
-    console.log(data)
+    localStorage.setItem("loggedin", true)
      
     return null
 }
